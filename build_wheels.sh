1ef652e162204f40b92d950e64c7ab4418e20037
Dimiter Naydenov <dimiter@users.noreply.github.com>
2019-10-29 16:01:11 +0200

    Fixed build script paths

--- build_wheels.sh
+++ build_wheels.sh
@@ -4,13 +4,13 @@ set -e -x
 # Install a system package required by our library
 yum install -y libxslt-devel
 
-/usr/bin/env
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
+    set +x
     if [ "$(basename $(dirname ${PYBIN}) | cut -d'-' -f1)x" = "cp27x" ]; then
         continue
     fi
+
     if [ "$(basename $(dirname ${PYBIN}) | cut -d'-' -f1)x" = "cp34x" ]; then
         continue
     fi
@@ -18,11 +18,12 @@ for PYBIN in /opt/python/*/bin; do
     if [ "$(basename $(dirname ${PYBIN}) | cut -d'-' -f1)x" = "cp35x" ]; then
         continue
     fi
+    set -x
 
     "${PYBIN}/pip" install -U pip setuptools wheel
-    "${PYBIN}/pip" install -r requirements.txt
-    "${PYBIN}/python" setup.py sdist bdist_wheel
-    auditwheel repair dist/*.whl --plat $PLAT -w dist/
-    "${PYBIN}/pip" install xmlstarlet --no-index -f dist && "${PYBIN}/pytest" -v
-    "${PYBIN}/twine" check dist/* && "${PYBIN}/twine" upload dist/*
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/python" /io/setup.py sdist bdist_wheel
+    auditwheel repair /io/dist/*.whl --plat $PLAT -w /io/dist/
+    "${PYBIN}/pip" install xmlstarlet --no-index -f /io/dist && "${PYBIN}/pytest" -v
+    "${PYBIN}/twine" check /io/dist/* && "${PYBIN}/twine" upload io/dist/*
 done
