8fb7745ed811251e69173aec571c185722a6059f
Dimiter Naydenov <dimiter@naydenov.net>
2020-10-04 18:32:53 +0300

    Bump version: 1.6.5 → 1.6.6

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u"Mikhail Grushinskiy"
 # the built documents.
 #
 # The short X.Y version.
-version = "1.6.5"
+version = "1.6.6"
 # The full version, including alpha/beta/rc tags.
 release = version
 
