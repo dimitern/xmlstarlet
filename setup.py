715bfae3e84b793f86db37655f78c240d23bbc19
Dimiter Naydenov <dimitern@users.noreply.github.com>
2020-10-04 22:02:39 +0300

    Post v1.6.6 (#135)
    * Bump version: 1.6.5 → 1.6.6
    
    * Update README with GHW link & badge, credits

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/dimitern/xmlstarlet",
-    version="1.6.5",
+    version="1.6.6",
     zip_safe=False,
 )
